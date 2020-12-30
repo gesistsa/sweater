8d371fce92c843e184623a00c4f73f5dd18af9ac
chainsawriot <chainsawtiney@gmail.com>
2020-12-30 15:18:46 +0100

    Make rnd type safe

--- R/rnd.R
+++ R/rnd.R
@@ -6,8 +6,8 @@
 
 #' @export
 rnd <- function(w, S, A, B) {
-    v1 <- apply(w[A,], 2, mean)
-    v2 <- apply(w[B,], 2, mean)
+    v1 <- purrr::map_dbl(purrr::array_branch(w[A,], 2), mean)
+    v2 <- purrr::map_dbl(purrr::array_branch(w[B,], 2), mean)
     norm_a <- purrr::map_dbl(S, .get_dist, v1, w)
     norm_b <- purrr::map_dbl(S, .get_dist, v2, w)
     norm_diff <- norm_a - norm_b
