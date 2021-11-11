5a3d15df42dc6eb1e2fe739436ce17a7107299e5
chainsawriot <chainsawtiney@gmail.com>
2021-11-11 17:14:24 +0100

    Add test cases for `weat_resampling()`

---
+++ tests/testthat/test-weat_resampling.R
@@ -0,0 +1,17 @@
+set.seed(11111)
+S <- c("math", "algebra", "geometry", "calculus", "equations", "computation", "numbers", "addition")
+T <- c("poetry", "art", "dance", "literature", "novel", "symphony", "drama", "sculpture")
+A <- c("male", "man", "boy", "brother", "he", "him", "his", "son")
+B <- c("female", "woman", "girl", "sister", "she", "her", "hers", "daughter")
+
+sw <- weat(glove_math, S, T, A, B)
+
+test_that("resampling", {
+    expect_error(weat_resampling(S))
+    expect_error(weat_resampling(sw), NA)
+    res <- weat_resampling(sw)
+    expect_equal(class(res), "htest")
+    tol <- 0.01
+    exp <- 0.018 #from Caliskan paper
+    expect_true(abs(res$p.value - exp) < tol)
+})
